92e678714b08c42d704128c16d0d4ede0141c09a
Johanna <jjea.aquino@gmail.com>
2023-09-27 19:41:52 +0800

    updated files

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -3,9 +3,7 @@ module.exports = {
     description: 'Short how-to-guide for updating sizes in shopify store.',
     themeConfig: {
         nav: [
-            {text: 'Home', link: '/home/'},
             {text: 'Guide', link: '/guide/'},
-            {text: 'About Me', link: 'https://drive.google.com/file/d/1jxR87kx1KIFJQ6pK11bjEMZqb70yfV_x/view?usp=drive_link'},
         ]
     },
     
