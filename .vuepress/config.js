23d2bdc3afaff51d4d5c0966c5497b323b403b6f
Johanna <jjea.aquino@gmail.com>
2023-09-27 12:55:52 +0800

    New Test Repository
    Added all the files

---
+++ .vuepress/config.js
@@ -0,0 +1,12 @@
+module.exports = { 
+    title: 'Application Test',
+    description: 'Short how-to-guide for updating sizes in shopify store.',
+    themeConfig: {
+        nav: [
+            {text: 'Home', link: '/home/'},
+            {text: 'Guide', link: '/guide/'},
+            {text: 'About Me', link: 'https://drive.google.com/file/d/1jxR87kx1KIFJQ6pK11bjEMZqb70yfV_x/view?usp=drive_link'},
+        ]
+    },
+    
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+
+# Application-Test
+This is created for the application I have for Rakwireless
